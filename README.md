c10428634c7dee06d347d5296e5606491eb43e27
Gerardo <gerardo.garcia@gigas.com>
2019-12-27 16:31:21 +0100

    help added

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ To get the list of all tag of one image:
 exec:
 
 ```
-sh docker_image_tag_search.sh IMAGE_NAME
+bash docker_image_tag_search.sh IMAGE_NAME
 ```
 
 If you want to limit the number of results you have to set the max_page_number executing:
 
 ```
-sh docker_image_tag_search.sh IMAGE_NAME PAGE_NUMBER
+bash docker_image_tag_search.sh IMAGE_NAME PAGE_NUMBER
 ```
--- docker_image_tag_search.sh
+++ docker_image_tag_search.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 
-i=0
-is_num='^-?[0-9]+([.][0-9]+)?$'
-if  <p class="wiki-page-missing">$2 =~ $is_num</p> ; then
-        max_pages=$2
-else
-        max_pages=20
+URL="https://registry.hub.docker.com/v2/repositories/library/$1/tags/?page="
+
+
+function credits {
+    echo -e "docker_image_tag_search"
+    echo -e "by Gerardo G. Urtiaga"
+    echo -e "2019 (C) AprendeIT\n\n"
+}
+
+function showhelp {
+    credits
+    echo -e "You have used a wrong syntax"
+    echo -e "You can use this script as follows:\n"
+    echo -e "\tbash docker_image_tag_search.sh IMAGE_NAME"
+    echo -e "\nIf you want to limit the number of results you have to set the max_page_number executing:"
+    echo -e "\tbash docker_image_tag_search.sh IMAGE_NAME PAGE_NUMBER"
+}
+
+
+
+#Check if entry point is empty
+if [ -z "$1" ]
+then
+    showhelp
+    exit
 fi
 
+
+is_num='^-?[0-9]+([.][0-9]+)?$'
+if  [[ $2 =~ $is_num ]]
+then
+    max_pages=$2
+else
+    max_pages=1
+fi
+
+i=0
 for i in $(seq 0 $max_pages)
 do
-   i=$((i+1))
-   timeout 10 curl https://registry.hub.docker.com/v2/repositories/library/$1/tags/?page=$i 2>/dev/null|jq '."results"[]["name"]'
+    i=$((i+1))
+    timeout 10 curl $URL$i 2>/dev/null|jq '."results"[]["name"]'
 
 done
+
